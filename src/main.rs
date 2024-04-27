ef5eb27005af80521a665d238270249de8f6020d
lukki <nolan0707@gmail.com>
2024-04-27 13:40:26 +0800

    feature: add csv support

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
 use clap::Parser;
-// rcli csv -i input.csv -o output.csv --hearer -d ','
+use rcli::process_csv;
+use rcli::{Opts, SubCommand};
 
-#[derive(Debug, Parser)]
-#[command(name = "rcli", version, author,about,long_about=None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "show CSV, or convert CSV to other formats.")]
-    Csv(CsvOpts),
-}
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-fn main() {
+fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
-    println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(ops) => process_csv(&ops.input, &ops.output)?,
+    }
+    Ok(())
 }
