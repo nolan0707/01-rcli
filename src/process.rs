a0eb7f3ee87df45660347fc2c31659095fe660fe
lukki <nolan0707@gmail.com>
2024-04-27 14:16:58 +0800

    feature: support common csv file

--- src/process.rs
+++ src/process.rs
@@ -3,6 +3,7 @@ use std::fs;
 use anyhow::{Ok, Result};
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
@@ -18,10 +19,12 @@ struct Player {
 
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
+    let headers = reader.headers()?.clone();
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
     let json = serde_json::to_string_pretty(&ret)?;
     fs::write(output, json)?;
